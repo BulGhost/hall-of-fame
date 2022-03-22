ff88a14a3d1b90c2552676837e7323e4016914f6
Dmitriy Bulynko <bulynko.dmitriy@gmail.com>
2022-03-22 11:51:43 +0700

    Dependency Injection for Business logic

---
+++ HallOfFame.BusinessLogic/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using FluentValidation;
+using HallOfFame.BusinessLogic.Common.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HallOfFame.BusinessLogic
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessLogic(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            ValidatorOptions.Global.LanguageManager.Enabled = false;
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
