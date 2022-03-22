9ae7ef7ce24c651664efd9a2d5a0f30d80b2b0d5
Dmitriy Bulynko <bulynko.dmitriy@gmail.com>
2022-03-22 11:44:25 +0700

    Logging added in behavior pipeline

---
+++ HallOfFame.BusinessLogic/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace HallOfFame.BusinessLogic.Common.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest
+    : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+
+    public LoggingBehavior(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        string requestName = typeof(TRequest).Name;
+        string jsonRequest = JsonSerializer.Serialize(request);
+        _logger.LogInformation($"Request: {requestName} | {jsonRequest}");
+        return await next();
+    }
+}
