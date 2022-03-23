9570c3bfa9544ed754082fbbec0fdcf9b59ff6b8
Dmitriy Bulynko <bulynko.dmitriy@gmail.com>
2022-03-23 11:28:02 +0700

    Test data initializer added

---
+++ HallOfFame.DataAccess/Initialization/TestData.cs
@@ -0,0 +1,26 @@
+﻿using HallOfFame.DataAccess.Models;
+
+namespace HallOfFame.DataAccess.Initialization;
+
+public static class TestData
+{
+    public static List<PersonModel> Persons => new()
+    {
+        new PersonModel { Id = 1, Name = "August Haynes", DisplayName = "A_Haynes" },
+        new PersonModel { Id = 2, Name = "Javier Hardin", DisplayName = "J_Hardin" },
+        new PersonModel { Id = 3, Name = "Kristopher Black", DisplayName = "K_Black" }
+    };
+
+    public static List<SkillModel> Skills => new()
+    {
+        new SkillModel { PersonId = 1, Name = "C#", Level = 9 },
+        new SkillModel { PersonId = 1, Name = ".NET", Level = 8 },
+        new SkillModel { PersonId = 1, Name = "EF Core", Level = 6 },
+        new SkillModel { PersonId = 1, Name = "Git", Level = 5 },
+        new SkillModel { PersonId = 2, Name = "C#", Level = 7 },
+        new SkillModel { PersonId = 2, Name = "EF Core", Level = 5 },
+        new SkillModel { PersonId = 2, Name = "WinForms", Level = 3 },
+        new SkillModel { PersonId = 3, Name = "C#", Level = 5 },
+        new SkillModel { PersonId = 3, Name = ".NET", Level = 4 }
+    };
+}
---
+++ HallOfFame.DataAccess/Initialization/TestDataInitializer.cs
@@ -0,0 +1,65 @@
+﻿using HallOfFame.DataAccess.DbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace HallOfFame.DataAccess.Initialization;
+
+public static class TestDataInitializer
+{
+    public static async Task Initialize(HallOfFameDbContext context)
+    {
+        await DropAndCreateDatabase(context);
+        await SeedData(context);
+    }
+
+    internal static async Task DropAndCreateDatabase(HallOfFameDbContext context)
+    {
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.MigrateAsync();
+    }
+
+    internal static async Task SeedData(HallOfFameDbContext context)
+    {
+        try
+        {
+            await ProcessInsert(context, context.Persons, TestData.Persons);
+            await ProcessInsert(context, context.Skills, TestData.Skills, false);
+        }
+        catch (Exception)
+        {
+            await context.DisposeAsync();
+            throw;
+        }
+    }
+
+    private static async Task ProcessInsert<TEntity>(HallOfFameDbContext context, DbSet<TEntity> table,
+        List<TEntity> records, bool hasIdentityKey = true) where TEntity : class
+    {
+        if (await table.AnyAsync()) return;
+
+        IExecutionStrategy strategy = context.Database.CreateExecutionStrategy();
+        await strategy.ExecuteAsync(async () =>
+        {
+            await using IDbContextTransaction transaction = await context.Database.BeginTransactionAsync();
+            try
+            {
+                IEntityType metaData = context.Model.FindEntityType(typeof(TEntity).FullName!);
+                string schemaName = metaData!.GetSchema() ?? "dbo";
+                string tableName = metaData.GetTableName();
+                if (hasIdentityKey)
+                    await context.Database.ExecuteSqlRawAsync($"SET IDENTITY_INSERT {schemaName}.{tableName} ON");
+                await table.AddRangeAsync(records);
+                await context.SaveChangesAsync();
+                if (hasIdentityKey)
+                    await context.Database.ExecuteSqlRawAsync($"SET IDENTITY_INSERT {schemaName}.{tableName} OFF");
+                await transaction.CommitAsync();
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        });
+    }
+}
