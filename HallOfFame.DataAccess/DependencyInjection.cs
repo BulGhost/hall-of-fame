3d958e0b3cba273da12c38b1aa85604c53531163
Dmitriy Bulynko <bulynko.dmitriy@gmail.com>
2022-03-23 12:03:02 +0700

    Dependency Injection for Data access

---
+++ HallOfFame.DataAccess/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using HallOfFame.DataAccess.DbContext;
+using HallOfFame.DataAccess.Repositories;
+using HallOfFame.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HallOfFame.DataAccess;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDataAccess(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<HallOfFameDbContext>(options =>
+            options.UseSqlServer(connectionString,
+                optionsBuilder => optionsBuilder.EnableRetryOnFailure()));
+
+        services.AddScoped<IPersonRepo, PersonRepo>();
+        return services;
+    }
+}
